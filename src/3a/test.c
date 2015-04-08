b2570fcd8e8c69800daba1f0067305803f37b985
Franesco Agosti <fra5@duke.edu>
2015-04-07 20:52:31 -0400

    added tests

---
+++ src/3a/test.c
@@ -0,0 +1,24 @@
+//
+//  test.c
+//  TCP2
+//
+//  Created by Francesco on 4/7/15.
+//  Copyright (c) 2015 FrancescoAgosti. All rights reserved.
+//
+
+#include <stdio.h>
+#include "reliable.c"
+#include "packet_list.c"
+#include "constants.h"
+#include "rlib.h"
+
+
+int
+main (int argc, char **argv) {
+    
+    packet_t* packet = (packet_t*) malloc(MAX_PACKET_SIZE);
+    
+    
+    
+    
+}
