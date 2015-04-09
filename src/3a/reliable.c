1c8eb5daa536247a70bf27a599f9acbb1327d24e
Franesco Agosti <fra5@duke.edu>
2015-04-08 21:28:03 -0400

    more print statements

--- src/3a/reliable.c
+++ src/3a/reliable.c
@@ -495,12 +495,10 @@ void resend_packets(rel_t *rel) {
 */
 	packet_list* packets_iter = rel->send_buffer;
 	while (packets_iter && packets_iter->packet) {
-/*
+
 #ifdef DEBUG
-		fprintf(stderr, "\n--- Resending packet %d ---\n", ntohl(packets_iter->packet->seqno));
+		fprintf(stderr, "Resending packet: \n", ntohl(packets_iter->packet->seqno));
 #endif
-*/
-        
 		conn_sendpkt(rel->c, packets_iter->packet, ntohs(packets_iter->packet->len));
 		packets_iter = packets_iter->next;
 	}
